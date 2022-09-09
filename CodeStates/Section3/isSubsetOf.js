afcf7d8bdd10100a90d5a73ab5e26100ebea1485
Miro <asc9602@naver.com>
2022-09-09 09:44:20 +0900

    Create isSubsetOf.js
    코드스테이츠 Section3 isSubsetOf

---
+++ CodeStates/Section3/isSubsetOf.js
@@ -0,0 +1,26 @@
+const isSubsetOf = function (base, sample) {
+    // 못 풀었음 레퍼런스 코드 이해하기
+  
+    // naive solution: O(M * N)
+    // return sample.every((item) => base.includes(item));
+  
+    // 각 배열을 정렬: O(N * logN), O(M * logM)
+    // N >= M 이므로, O(N * logN)
+    base.sort((a, b) => a - b);
+    sample.sort((a, b) => a - b);
+  
+    const findItemInSortedArr = (item, arr, from) => {
+      for (let i = from; i < arr.length; i++) {
+        if (item === arr[i]) return i;
+        else if (item < arr[i]) return -1;
+      }
+      return -1;
+    };
+  
+    let baseIdx = 0;
+    for (let i = 0; i < sample.length; i++) {
+      baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
+      if (baseIdx === -1) return false;
+    }
+    return true;
+  };  
