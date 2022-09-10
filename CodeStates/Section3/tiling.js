cd5129d164a435f46e57b86e97d54568ca6dc96d
Miro <asc9602@naver.com>
2022-09-10 11:58:44 +0900

    Create tiling.js
    코드스테이츠 Section3 tiling

---
+++ CodeStates/Section3/tiling.js
@@ -0,0 +1,14 @@
+let tiling = function (n) {
+    // 못 풀었음 다시 풀어볼 것
+    const memo = [0, 1, 2];
+  
+    // 재귀를 위한 보조 함수(auxiliary function)을 선언)
+    const aux = (size) => {
+      // 이미 해결한 문제는 풀지 않는다.
+      if (memo[size] !== undefined) return memo[size];
+      if (size <= 2) return memo[n];
+      memo[size] = aux(size - 1) + aux(size - 2);
+      return memo[size];
+    };
+    return aux(n);
+  };
