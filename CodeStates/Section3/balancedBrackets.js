6463d6a5af1ceb34bfae85529d67b9f81fbd8bb1
Miro <asc9602@naver.com>
2022-09-16 18:02:41 +0900

    Update balancedBrackets.js
    코드스테이츠 Section3 balancedBrackets naive solution 추가

--- CodeStates/Section3/balancedBrackets.js
+++ CodeStates/Section3/balancedBrackets.js
@@ -21,3 +21,23 @@ const balancedBrackets = function (str) {
   
     return stack.length === 0;
   };
+
+// naive solution
+// const balancedBrackets = function (str) {
+//   const stack = [];
+//   const opener = '(';
+//   const closer = ')';
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === opener) {
+//       stack.push(str[i]);
+//     } else if (str[i] === closer) {
+//       const top = stack.pop();
+//       if (top !== opener) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
