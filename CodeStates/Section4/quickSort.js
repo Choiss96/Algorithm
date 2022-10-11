5435b7eb3612e725c0e0b5a5a120403afe2568a2
Miro <asc9602@naver.com>
2022-10-11 16:59:20 +0900

    Update quickSort.js
    코드스테이츠 Section4 quickSort naive solution 추가

--- CodeStates/Section4/quickSort.js
+++ CodeStates/Section4/quickSort.js
@@ -14,3 +14,21 @@ function quickSort(arr, transform = (item) => item) {
     const rSorted = quickSort(right, transform);
     return [...lSorted, pivot, ...rSorted];
   }
+  
+  // naive solution
+  // const quickSort = function (arr) {
+  //   if (arr.length <= 1) return arr;
+  
+  //   const pivot = arr[0];
+  //   const left = [];
+  //   const right = [];
+  
+  //   for (let i = 1; i < arr.length; i++) {
+  //     if (arr[i] <= pivot) left.push(arr[i]);
+  //     else right.push(arr[i]);
+  //   }
+  
+  //   const lSorted = quickSort(left);
+  //   const rSorted = quickSort(right);
+  //   return [...lSorted, pivot, ...rSorted];
+  // };
